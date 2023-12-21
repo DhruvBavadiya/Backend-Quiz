b27c0bce33df6e848478ed885893e09ed9e1907a
Dhruv Bavadiya <dhruvbavadiya99@gmail.com>
2023-12-21 15:15:07 +0530

    added data

--- controller/quizController.js
+++ controller/quizController.js
@@ -4,16 +4,16 @@ const NodeCache = require("node-cache");
 const myCache = new NodeCache();
 
 exports.addQuiz = async (req, res, next) => {
-  const data = req.body;
-  if (!Object.keys(data).length) {
-    res.status(403).json({
-      success: false,
-      message: "No Data found",
-    });
-  } else {
+  try {
+    const data = req.body;
+    if (!Object.keys(data).length) {
+      return res.status(403).json({
+        success: false,
+        message: "No Data found",
+      });
+    }
+
     const sectionId = data.sectionId;
-    // console.log(sectionId);
-    // Find the section document by ID
     const section = await Section.findOne({ sectionId: sectionId });
 
     if (section) {
@@ -22,7 +22,6 @@ exports.addQuiz = async (req, res, next) => {
       let numOfquiz = section.numberOfQuestions || 0;
       numOfquiz += data.questions.length;
 
-      // Update the numberOfQuestions property on the specific section document
       await Section.updateOne(
         { sectionId: sectionId },
         {
@@ -32,110 +31,133 @@ exports.addQuiz = async (req, res, next) => {
       );
 
       if (quiz) {
-        res.status(200).json({
+        return res.status(200).json({
           success: true,
           quiz,
         });
       } else {
-        res.status(403).json({
+        return res.status(403).json({
           success: false,
           message: "Failed to create quiz",
         });
       }
     } else {
-      res.status(403).json({
+      return res.status(403).json({
         success: false,
         message: "No section id found",
       });
     }
+  } catch (error) {
+    next(error);
   }
 };
 
 exports.addSection = async (req, res, next) => {
-  const data = req.body;
-  data.numberOfQuestions = 0;
-  const numberOfQuestions = 0;
-  if (!Object.keys(data).length) {
-    res.status(403).json({
-      success: false,
-      message: "No Data found",
+  try {
+    const data = req.body;
+    data.numberOfQuestions = 0;
+    const numberOfQuestions = 0;
+    if (!Object.keys(data).length) {
+      return res.status(403).json({
+        success: false,
+        message: "No Data found",
+      });
+    }
+
+    const section = await Section.create(data);
+    myCache.del("section");
+    myCache.del("trending-section");
+
+    if (!section) {
+      return res.status(403).json({
+        success: false,
+        message: "some error while adding section",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      section,
     });
+  } catch (error) {
+    next(error);
   }
-  const section = await Section.create(data);
-  myCache.del("section");
-  myCache.del("trending-section");
-  if (!section) {
-    res.status(403).json({
-      success: false,
-      message: "some error while adding section",
-    });
-  }
-  // Section.numberOfQuestions = numberOfQuestions;
-  // await section.save()
-  res.status(200).json({
-    success: true,
-    section,
-  });
 };
 
 exports.getBycategory = async (req, res, next) => {
-  const section = await Section.findOne({ category: req.query.category });
+  try {
+    const section = await Section.findOne({ category: req.query.category });
 
-  // console.log(section);
-  if (!section || !section.questions || section.questions.length === 0) {
-    return res.status(404).json({
-      success: false,
-      message: "Category not found or no questions available for the category.",
-    });
-  }
-  let question = section.questions;
-  if (req.query.difficulty) {
-    question = section.questions.filter(
-      (question) => question.difficulty == req.query.difficulty
-    );
-  }
-
-  const shuffledQuestions = question.sort(() => Math.random() - 0.5);
-  const randomQuestions = shuffledQuestions.slice(0, 11);
-
-  let answers = [];
-
-  randomQuestions.forEach((question) => {
-    answers.push({
-      questionId: question.questionId,
-      correctOption: question.correctOption,
-    });
-  });
-
-  res
-    .status(200)
-    .json({ success: true, questions: randomQuestions, answer: answers });
-};
-
-exports.getTrending = async (req, res, next) => {
-  let section = [];
-  if (myCache.has("trending-section")) {
-    section = JSON.parse(myCache.get("trending-section"));
-  } else {
-    section = await Section.find({ isTrending: true });
-    if (!section) {
+    if (!section || !section.questions || section.questions.length === 0) {
       return res.status(404).json({
         success: false,
         message:
           "Category not found or no questions available for the category.",
       });
     }
-    myCache.set("trending-section", JSON.stringify(section));
-  }
 
-  res.status(200).json({ success: true, section });
+    let question = section.questions;
+    if (req.query.difficulty) {
+      question = section.questions.filter(
+        (question) => question.difficulty == req.query.difficulty
+      );
+    }
+
+    const shuffledQuestions = question.sort(() => Math.random() - 0.5);
+    const randomQuestions = shuffledQuestions.slice(
+      0,
+      shuffledQuestions.length
+    );
+
+    let answers = [];
+
+    randomQuestions.forEach((question) => {
+      answers.push({
+        questionId: question.questionId,
+        correctOption: question.correctOption,
+      });
+    });
+
+    res.status(200).json({
+      success: true,
+      questions: randomQuestions,
+      length: randomQuestions.length,
+      answer: answers,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getTrending = async (req, res, next) => {
+  try {
+    let section = [];
+    if (myCache.has("trending-section")) {
+      section = JSON.parse(myCache.get("trending-section"));
+    } else {
+      section = await Section.find({ isTrending: true });
+      if (!section) {
+        return res.status(404).json({
+          success: false,
+          message:
+            "Category not found or no questions available for the category.",
+        });
+      }
+      myCache.set("trending-section", JSON.stringify(section));
+    }
+
+    res.status(200).json({
+      success: true,
+      section,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 exports.getSections = async (req, res, next) => {
-  let section = [];
-  if (myCache.has("section")) {
-    section = JSON.parse(myCache.get("section"));
-  } else {
+  try {
+    let section = [];
     section = await Section.find();
     if (!section) {
       return res.status(404).json({
@@ -144,11 +166,12 @@ exports.getSections = async (req, res, next) => {
           "Category not found or no questions available for the category.",
       });
     }
-    myCache.set("section", JSON.stringify(section));
+
+    res.status(200).json({
+      success: true,
+      section,
+    });
+  } catch (error) {
+    next(error);
   }
-
-  res.status(200).json({ success: true, section });
 };
-
-
-
